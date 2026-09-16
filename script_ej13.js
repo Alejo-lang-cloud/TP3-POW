39b246d54286f278d0803817eb6b787e312fb25e
Alejo Hidalgo <ajjhidalgo613@gmail.com>
2026-09-16 19:21:42 -0300

    logica calculadora lista!!

--- script_ej13.js
+++ script_ej13.js
@@ -2,7 +2,7 @@
 const displayAcum = document.querySelector('.contenedor-display-acum');
 const displayNumActual = document.querySelector('.contenedor-display-numactual');
 
-let operandoActual = '0'; 
+let operandoActual = ''; 
 let operandoAnterior = '';
 let operacion = null; /*inicialmente*/
 
@@ -54,7 +54,7 @@ function calcular() {
             resultado = anterior * actual;
             break;
         case '/':
-            resultado = actual === 0 ? 'Error' : anterior / actual; /*verificamos la division por cero*/ 
+            resultado = actual === 0 ? 'Sintax Error' : anterior / actual; /*verificamos la division por cero*/ 
             break;
         default:
             return;
@@ -65,4 +65,43 @@ function calcular() {
     operandoAnterior = '';
     actualizarDisplay(); 
 }
+
+// botones numericos: event listeners 
+document.querySelectorAll('.boton-numero').forEach(
+    boton => {boton.addEventListener('click', () => agregarNumero(boton.innerText))}); /*boton es una variable temporal, recibe el evento del click*/ 
+
+// botones de operaciones: event listeners
+document.querySelectorAll('.boton-operador').forEach(boton => {boton.addEventListener('click', () => {
+    const op = boton.innerText;
+    if (['+','-','x','/'].includes(op)) {
+        elegirOperacion(op);
+    }else if (op === '±') {
+        operandoActual = (parseFloat(operandoActual) * -1).toString(); //para cambiar de signo
+        actualizarDisplay();
+    } else if (op === '%') {
+        operandoActual = (parseFloat(operandoActual) / 100).toString(); //pasamos a porcentaje
+        actualizarDisplay();
+        }
+    });
+});
+
+// boton igual: event listener
+document.getElementById('igual').addEventListener('click', calcular);
+
+// boton C (borra todo)
+document.getElementById('C').addEventListener('click', () => {
+    operandoActual = '';
+    operandoAnterior = '';
+    operacion = null;
+    actualizarDisplay();
+});
+
+//boton CE (borra la ENTRADA ACTUAL)
+document.getElementById('CE').addEventListener('click', () => {
+    operandoActual = '0';
+    actualizarDisplay();
+});
+
+
+actualizarDisplay();
 
